d97db38e4d5c30713832a79fb9ae5e008118f9b2
Luca Lowndes <104835046+LucaLow@users.noreply.github.com>
2024-08-07 15:56:47 +1000

    Implemented Leaderboard system
    Need to add a way to add to the leaderboard file, this will be implemented later in the games development cycle.

---
+++ Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using System.IO;
+
+public class Leaderboard : MonoBehaviour
+{
+    
+    public TextMeshProUGUI leaderboardText;
+    public List<Tuple<int, string>> leaderboard = new List<Tuple<int, string>>();
+    void Start()
+    {
+        // read leaderboard file
+        StreamReader sr = new StreamReader("Assets/Scripts/leaderboard.txt");
+        string line;
+        //read each line of the file into the line variable
+        while ((line= sr.ReadLine())!= null)
+        {
+            Debug.Log(line);
+            // add each line to the list
+            leaderboard.Add(
+                new Tuple<int, string>(
+                    Convert.ToInt16(line.Split(',')[1]), 
+                    line.Split(',')[0]
+                    ));
+        }
+        
+        // To be used to compare the value of each score
+        int compare(Tuple<int, string> x, Tuple<int, string> y)
+        {
+            return y.Item1.CompareTo(x.Item1);
+        }
+        leaderboard.Sort(compare);
+        string leaderboardString = "";
+        foreach(Tuple<int, string> s in leaderboard)
+        {
+            leaderboardString += Convert.ToString(s.Item1) + ", " + Convert.ToString(s.Item2) + "\n";
+        }
+        // write the leaderboard string into the scene
+        leaderboardText.text = leaderboardString;
+        Debug.Log(leaderboard);
+        Debug.Log(leaderboard[0]);
+    }
+}
