e8fe4ea40f98b422e9d6333233443cf01425e6ce
Luca Lowndes <104835046+LucaLow@users.noreply.github.com>
2024-09-18 15:29:11 +1000

    wave updates

--- Assets/Scripts/UI/Leaderboard.cs
+++ Assets/Scripts/UI/Leaderboard.cs
@@ -12,20 +12,26 @@ public class Leaderboard : MonoBehaviour
     public List<Tuple<int, string>> leaderboard = new List<Tuple<int, string>>();
     void Start()
     {
+       // if(!File.Exists("C:\\leaderboard.text")) 
+       //     using (StreamWriter sw = File.CreateText(Application.persistentDataPath + "leaderboard.text"))
+        //    {
+         //       sw.Write("");
+        //    }
+
         // read leaderboard file
-        StreamReader sr = new StreamReader("Assets/Scripts/UI/leaderboard.txt");
+        StreamReader sr = new StreamReader(Application.persistentDataPath+"leaderboard.txt");
         string line;
         //read each line of the file into the line variable
-        while ((line= sr.ReadLine())!= null)
-        {
-            Debug.Log(line);
+      //  while ((line= sr.ReadLine())!= null)
+      //  {
+      //      Debug.Log(line);
             // add each line to the list
-            leaderboard.Add(
-                new Tuple<int, string>(
-                    Convert.ToInt16(line.Split(',')[1]), 
-                    line.Split(',')[0]
-                    ));
-        }
+      //      leaderboard.Add(
+        //        new Tuple<int, string>(
+         //           Convert.ToInt16(line.Split(',')[1]), 
+          //          line.Split(',')[0]
+       //             ));
+       // }
         
         // To be used to compare the value of each score
         int compare(Tuple<int, string> x, Tuple<int, string> y)
