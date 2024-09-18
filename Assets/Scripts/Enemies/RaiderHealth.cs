8c5b52aa7adf2f651b45f87bf46c600bb72ebd34
Nathan <127276547+NathanTavaria@users.noreply.github.com>
2024-09-18 15:35:15 +1000

    viking raiders

--- Assets/Scripts/Enemies/RaiderHealth.cs
+++ Assets/Scripts/Enemies/RaiderHealth.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyHealth : MonoBehaviour
+public class RaiderHealth : MonoBehaviour
 {
+    public int RaidersHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
